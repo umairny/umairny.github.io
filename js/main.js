a0a3a7fa5e68f92b85e6c0685b4ff5dfc1e504eb
umairny <umairny1@gmail.com>
2020-11-10 20:45:17 -0500

    newweb

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) { 
-    current[0].className = current[0].className.replace(" active", "");
-  }
-  this.className += " active";
-  });
-}
+//# menu bar #//
 
+(function () {
+  var Nav;
+
+  Nav = {
+    init: function () {
+      this.setup();
+      return this.uiBind();
+    },
+    setup: function () {
+      return $('#mainnav').find('li:not(:first-child)').toggleClass('invisible');
+    },
+    uiBind: function () {
+      return $(document).on('click', '#mainnav', function (e) {
+        return $(this).find('li:not(:first-child)').toggleClass('animate').toggleClass('invisible');
+      });
+    } };
+  Nav.init();
+}).call(this);
--- js/work.js
+++ js/work.js
@@ -1,63 +1,10 @@
-//gallery work
-const section = document.querySelector('#gal');
+$('#projects > li').hover(function(){
+  $(this).siblings().addClass('blur');
+}, function(){
+  $(this).removeClass('clicked').siblings().removeClass('blur');
+  
+});
 
-let currentPos = window.pageYOffset;
-
-const update = () => {
-	const newPos = window.pageYOffset;
-	const diff = newPos - currentPos;
-	const speed = diff * 0.25;
-	
-  section.style.transform = `skewY(${speed}deg)`;
-  section.style.transform = `skewX(${ speed }-deg)`;
-	
-	currentPos = newPos;
-	
-	requestAnimationFrame(update);
-}
-
-update();
-
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+$('#projects > li').click(function(e){
+  $(this).addClass('clicked');
+});

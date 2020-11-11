b2e20823e1b4b41ee72b5138d3eac6928c17144c
umairny <umairny1@gmail.com>
2020-11-10 21:55:53 -0500

    newweb2nd

--- js/main.js
+++ js/main.js
@@ -18,3 +18,15 @@
     } };
   Nav.init();
 }).call(this);
+
+// Work Js project //
+$('#projects > li').hover(function () {
+  $(this).siblings().addClass('blur');
+}, function(){
+  $(this).removeClass('clicked').siblings().removeClass('blur');
+  
+});
+
+$('#projects > li').click(function(e){
+  $(this).addClass('clicked');
+});

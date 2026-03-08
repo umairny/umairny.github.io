3b8f1b49aa1d4106b38062d59e43fa6ec2de8738
umairny <umairny1@gmail.com>
2026-03-08 12:33:51 -0400

    Add basename to BrowserRouter and fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
